9c1dc50f9bbc425a12e3a9ebd7aecde964728a46
DumpDos <donvitot@gmail.com>
2017-07-22 16:14:47 +0200

    Update metar.sh

--- metar.sh
+++ metar.sh
@@ -8,11 +8,10 @@
 # === Variables === #
 var='print'     #-- Variable impression
 varmet='metar'  #-- Variable metar
-varnot='notam'  #-- variable notam
+vartaf='taf'  #-- variable notam
 
 # === Variables d'entrées === #
 _oaci=LFPO   #-- Code oaci de l'aeroport
-
 _print=false #-- Condition impression
 
 #####################################################################$
@@ -34,22 +33,33 @@ if [ $_data == $varmet ]
 then
 
 # === Téléchargement du fichier === #
-wget -q -O /home/pi/metar/$_oaci'_API.xml' 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString='$_oaci'&hoursBeforeN$
+wget -q -O /home/pi/metar/$_oaci'_metAPI.xml' 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString='$_oaci'&hoursBeforeNow=0.1'
 
 # === Récupération des données === #
-metars=$(sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_API.xml')
+metars=$(sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_metAPI.xml')
 
 # === Affichage des données === #
-sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_API.xml'
+sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_metAPI.xml'
 
 # === Inscription des données dans les fichiers === #
 echo -e $metars>> /home/pi/metar/metar_historique.txt
 echo -e $metars>> /home/pi/metar/metar_$_oaci.txt
 
-elif [ $_data == $varnot ]
+elif [ $_data == $vartaf ]
 then
 
-test
+# === Téléchargement du fichier === #
+wget -q -O /home/pi/metar/$_oaci'_tafAPI.xml' 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString='$_oaci'&hoursBeforeNow=0.1'
+
+# === Récupération des données === #
+taf=$(sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_tafAPI.xml')
+
+# === Affichage des données === #
+sed -n 's,.*<raw_text>\(.*\)</raw_text>,\1,p' /home/pi/metar/$_oaci'_tafAPI.xml'
+
+# === Inscription des données dans les fichiers === #
+echo -e $metars>> /home/pi/metar/tafs_historique.txt
+echo -e $metars>> /home/pi/metar/taf_$_oaci.txt
 
 fi
 
