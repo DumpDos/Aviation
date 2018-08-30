5d6cfc817e136e7b465919f36e79fa30b61bb06f
DumpDos <donvitot@gmail.com>
2018-08-30 12:48:40 +0200

    Update list.js

--- list.js
+++ list.js
@@ -1,1 +1,4 @@
-time = getAvailableLocales()
+time = getAvailableLocales(js.hour.format:"HH:MM:SS", js.date.format:"DD/MM/YYYY")
+local gmt.time = js.GetForm(hour)
+local gmt.date = js.date.format
+
