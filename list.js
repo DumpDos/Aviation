dc9b34235fc46cb844cf1f62661a43efa541fd85
DumpDos <donvitot@gmail.com>
2018-09-07 12:40:32 +0200

    Update list.js

--- list.js
+++ list.js
@@ -1,4 +1,19 @@
+# Format JSON #
+
+# Import #
+
+
+
+# Variables #
+local.url_ntp_form = {get.hourform="pool.ntp.org"}
+local.time = {jsonform =[^.]local.url_ntp_form}
+local.aircraft_list = {Lastdv}
+
+# Mise en forme temps #
 time = getAvailableLocales(js.hour.format:"HH:MM:SS", js.date.format:"DD/MM/YYYY")
 local gmt.time = js.GetForm(hour)
 local gmt.date = js.date.format
 
+# Parse code JSON #
+
+JSON.parse = 
